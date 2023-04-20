243e2de7231605fba9af0f2722d181f891b01fbf
skowront <tomasz.skowron98@gmail.com>
2023-04-20 14:52:49 +0200

    Add project files.

---
+++ SampleDotnetASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleDotnetASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
